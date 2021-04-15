841760db709f0cc27d2b00cfcd77866e15e737f1
Dima Kuiznietsov <dima282717799@gmail.com>
2021-04-16 00:11:20 +0300

    task with form

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 const $arena = document.querySelector('.arenas.arena1');
-const $button = document.querySelector('.button')
+// const $button = document.querySelector('.button')
+const $form = document.querySelector('.control');
+
+
+const HIT = {
+    head: 30,
+    body: 25,
+    foot: 20,
+}
+
+const ATTACK = ['head', 'body', 'foot'];
 
 
 const player1 = {
@@ -12,9 +22,9 @@ const player1 = {
     attack: function () {
         console.log(this.name + ' fight');
     },
-    changeHP: minusHP,
-    elHP: elHP,
-    renderHP: renderHP
+    changeHP,
+    elHP,
+    renderHP
 }
 
 
@@ -28,9 +38,9 @@ const player2 = {
     attack: function () {
         console.log(this.name + ' fight');
     },
-    changeHP: minusHP,
-    elHP: elHP,
-    renderHP: renderHP
+    changeHP,
+    elHP,
+    renderHP
 }
 
 function createElement(tag, className) {
@@ -71,51 +81,53 @@ function createPlayer(player) {
 $arena.appendChild(createPlayer(player1));
 $arena.appendChild(createPlayer(player2));
 
-function changeHP(player) {
-    let a = Math.ceil(Math.random() * 20);
-    player.changeHP(a);
-    let elem = player.elHP();
-    player.renderHP(elem)
-    // const $playerLife = document.querySelector('.player' + player.player + ' .life')
-    // player.hp -= Math.ceil(Math.random() * 20);
+// function changeHP(player) {
+//     let a = Math.ceil(Math.random() * 20);
+//     player.changeHP(a);
+//     let elem = player.elHP();
+//     player.renderHP(elem)
+// const $playerLife = document.querySelector('.player' + player.player + ' .life')
+// player.hp -= Math.ceil(Math.random() * 20);
 
-    // if (player.hp <= 0) {
-    //     player.hp = 0;
-    //     // $arena.appendChild(playerLose(player.name))
+// if (player.hp <= 0) {
+//     player.hp = 0;
+//     // $arena.appendChild(playerLose(player.name))
 
-    // }
-    // $playerLife.style.width = player.hp + '%';
-}
+// }
+// $playerLife.style.width = player.hp + '%';
+// }
 
-function minusHP(minus) {
+function changeHP(minus) {
     if (this.hp <= minus)
         this.hp = 0;
     else {
         this.hp -= minus;
     }
+
 }
 function elHP() {
+    console.log('this', this)
     const $playerLife = document.querySelector('.player' + this.player + ' .life')
-
     return $playerLife;
 }
 
-function renderHP(element) {
-    element.style.width = this.hp + '%';
+function renderHP() {
+    console.log('this.renderHP', this.elHP())
+    this.elHP().style.width = this.hp + '%';
 }
 
-$button.addEventListener('click', function () {
-    changeHP(player1);
-    changeHP(player2);
-    if (player1.hp > 0 && player2.hp == 0) {
-        $arena.appendChild(playerWins(player1.name))
+// $button.addEventListener('click', function () {
+//     changeHP(player1);
+//     changeHP(player2);
+//     if (player1.hp > 0 && player2.hp == 0) {
+//         $arena.appendChild(playerWins(player1.name))
 
-    }
-    else if (player2.hp > 0 && player1.hp == 0) {
-        $arena.appendChild(playerWins(player2.name))
+//     }
+//     else if (player2.hp > 0 && player1.hp == 0) {
+//         $arena.appendChild(playerWins(player2.name))
 
-    }
-})
+//     }
+// })
 
 function playerWins(name) {
     const $loseTitle = createElement('div', 'loseTitle')
@@ -134,4 +146,65 @@ function createReloadeButton() {
     })
     $divForButton.appendChild($button)
     return $divForButton
+}
+
+function getRandom(number) {
+    return Math.ceil(Math.random() * number);
+}
+
+function enemyAttack() {
+    const hit = ATTACK[getRandom(3) - 1]
+    const def = ATTACK[getRandom(3) - 1]
+
+    return {
+        value: getRandom(HIT[hit]),
+        hit,
+        def
+    }
+}
+
+$form.addEventListener('submit', function (e) {
+    e.preventDefault()
+
+    const enemy = enemyAttack();
+    console.log(enemy)
+
+    const attack = {}
+    for (let item of $form) {
+        if (item.checked && item.name === 'hit') {
+            attack.value = getRandom(HIT[item.value]);
+            attack.hit = item.value
+        }
+        if (item.checked && item.name === 'defence') {
+            attack.def = item.value
+        }
+
+        item.checked = false;
+    }
+    console.log(attack)
+    checkAttackDef(enemy, attack)
+})
+
+function checkAttackDef(enemy, attack) {
+    console.log('### enemy: ', enemy)
+    console.log('### attack: ', attack)
+    if (enemy.def != attack.hit) {
+        player2.changeHP(attack.value)
+        player2.renderHP(elHP())
+    }
+    else if (attack.def != enemy.attack) {
+        player1.changeHP(enemy.value)
+        player1.renderHP(elHP())
+    }
+    console.log('player1.hp ', player1.hp)
+    console.log('player2.hp ', player2.hp)
+    if (player2.hp >= attack.hit){
+        console.log(1)
+        playerWins(player1.name)
+    }
+    else if (player1.hp >= enemy.hit){
+        console.log(2)
+        playerWins(player2.name)
+    }
+
 }
